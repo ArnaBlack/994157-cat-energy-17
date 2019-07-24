68cdaf87173b3d29be1c2f268d37a74035f91607
Arna <sky-vamp@mail.ru>
2019-07-25 00:28:24 +0300

    заверщила хедер на главной

--- source/js/index.js
+++ source/js/index.js
@@ -1,1 +1,16 @@
 'use strict';
+
+( function () {
+  var navigation = document.querySelector(".main-nav");
+  var burger = navigation.querySelector(".main-nav__toggle");
+
+  navigation.classList.remove("main-nav--nojs");
+  navigation.classList.remove("main-nav--open");
+  navigation.classList.add("main-nav--closed");
+
+  burger.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    navigation.classList.toggle("main-nav--open");
+    navigation.classList.toggle("main-nav--closed");
+  });
+})();
