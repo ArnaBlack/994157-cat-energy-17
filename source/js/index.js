596f5ae21dec0dd2928d0c903bdb444b4c81e948
Arna <sky-vamp@mail.ru>
2019-07-27 16:47:07 +0300

    добавила яндекс карту

--- source/js/index.js
+++ source/js/index.js
@@ -1,6 +1,8 @@
 'use strict';
 
 ( function () {
+
+  //burger
   var navigation = document.querySelector(".main-nav");
   var burger = navigation.querySelector(".main-nav__toggle");
 
@@ -14,3 +16,26 @@
     navigation.classList.toggle("main-nav--closed");
   });
 })();
+
+  	// Яндекс карта
+    ymaps.ready(init);
+    function init() {
+      var map = new ymaps.Map('map', {
+        center: [59.9386, 30.3231],
+        zoom: 17//,
+        //controls: ['zoomControl'],
+        //behaviors: ['default', 'scrollZoom']
+      },  {
+        searchControlProvider: 'yandex#search'
+    });
+      var placemark = new ymaps.Placemark([59.9386,30.3231], {
+          hintContent: 'CatEnergy'
+        },
+        {
+          iconLayout: 'default#image', //название
+          iconImageHref: 'img/map-pin.png', //источник
+          iconImageSize: [113, 106], //размер
+          iconImageOffset: [-60, -106] //координаты смещения
+        });
+      map.geoObjects.add(placemark);
+      };
